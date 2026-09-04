mrsrimar22/android_kernel_qcom_sm8250

treewide: custom_project: Import xiaomi changes
The Patch based on QCOM release TAG:Snapdragon_Mid_2020.SPF.1.0.1R_r00024.0

From: spes-r-oss
Change-Id: Ia840bfd2e806c72b48ad426ff0b078214ec1625c
Signed-off-by: gongjingzhou <gongjingzhou@xiaomi.com>
Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

drivers/soc/qcom/subsys-pil-tz.c
@@ -33,6 +33,21 @@
 #define STOP_ACK_TIMEOUT_MS	1000
 #define CRASH_STOP_ACK_TO_MS	200
 
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+static char last_ssr_reason[MAX_SSR_REASON_LEN] = "none";
+static struct proc_dir_entry *last_ssr_reason_entry;
+
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#define CHECK_NV_DESTROYED_MI 1
+#ifdef CHECK_NV_DESTROYED_MI
+#include <linux/workqueue.h>
+#include <linux/slab.h>
+#define STR_NV_SIGNATURE_DESTROYED "CRITICAL_DATA_CHECK_FAILED"
+static char last_modem_sfr_reason[MAX_SSR_REASON_LEN] = "none";
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
+
 #define ERR_READY	0
 #define PBL_DONE	1
 
@@ -163,6 +178,95 @@ static struct msm_bus_scale_pdata scm_pas_bus_pdata = {
 	.usecase = scm_pas_bw_tbl,
 	.num_usecases = ARRAY_SIZE(scm_pas_bw_tbl),
 	.name = "scm_pas",
+};
+
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#ifdef CHECK_NV_DESTROYED_MI
+static struct kobject *checknv_kobj;
+static struct kset *checknv_kset;
+static const struct sysfs_ops checknv_sysfs_ops = {};
+
+static void kobj_release(struct kobject *kobj)
+{
+	kfree(kobj);
+}
+
+static struct kobj_type checknv_ktype = {
+	.sysfs_ops = &checknv_sysfs_ops,
+	.release = kobj_release,
+};
+
+static void checknv_kobj_clean(struct work_struct *work)
+{
+	kobject_uevent(checknv_kobj, KOBJ_REMOVE);
+	kobject_put(checknv_kobj);
+	kset_unregister(checknv_kset);
+}
+
+static void checknv_kobj_create(struct work_struct *work)
+{
+	int ret;
+
+	if (checknv_kset != NULL) {
+		pr_err("checknv_kset is not NULL, should clean up.");
+		kobject_uevent(checknv_kobj, KOBJ_REMOVE);
+		kobject_put(checknv_kobj);
+	}
+
+	checknv_kobj = kzalloc(sizeof(struct kobject), GFP_KERNEL);
+	if (!checknv_kobj) {
+		pr_err("kobject alloc failed.");
+		return;
+	}
+
+	if (checknv_kset == NULL) {
+		checknv_kset = kset_create_and_add("checknv_errimei", NULL, NULL);
+		if (!checknv_kset) {
+			pr_err("kset creation failed.");
+			goto free_kobj;
+		}
+	}
+
+	checknv_kobj->kset = checknv_kset;
+
+	ret = kobject_init_and_add(checknv_kobj, &checknv_ktype, NULL, "%s", "errimei");
+	if (ret) {
+		pr_err("%s: Error in creation kobject", __func__);
+		goto del_kobj;
+	}
+
+	kobject_uevent(checknv_kobj, KOBJ_ADD);
+	return;
+
+del_kobj:
+	kobject_put(checknv_kobj);
+	kset_unregister(checknv_kset);
+free_kobj:
+	kfree(checknv_kobj);
+}
+
+static DECLARE_DELAYED_WORK(create_kobj_work, checknv_kobj_create);
+static DECLARE_WORK(clean_kobj_work, checknv_kobj_clean);
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
+
+static int last_ssr_reason_proc_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%s\n", last_ssr_reason);
+	return 0;
+}
+
+static int last_ssr_reason_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, last_ssr_reason_proc_show, NULL);
+}
+
+static const struct file_operations last_ssr_reason_file_ops = {
+	.owner	= THIS_MODULE,
+	.open	= last_ssr_reason_proc_open,
+	.read	= seq_read,
+	.llseek	= seq_lseek,
+	.release = single_release,
 };
 
 static uint32_t scm_perf_client;
@@ -821,9 +925,12 @@ static void log_failure_reason(const struct pil_tz_data *d)
 		pr_err("%s SFR: (unknown, empty string found).\n", name);
 		return;
 	}
+	strlcpy(last_modem_sfr_reason, smem_reason, min(size, (size_t)MAX_SSR_REASON_LEN));
+	//pr_err("%s subsystem failure reason: %s.\n", name, last_modem_sfr_reason);
 
 	strlcpy(reason, smem_reason, min(size, (size_t)MAX_SSR_REASON_LEN));
 	pr_err("%s subsystem failure reason: %s.\n", name, reason);
+	snprintf(last_ssr_reason, (size_t)MAX_SSR_REASON_LEN, "%s: %s", name, reason);
 }
 
 static int subsys_shutdown(const struct subsys_desc *subsys, bool force_stop)
@@ -906,6 +1013,14 @@ static irqreturn_t subsys_err_fatal_intr_handler (int irq, void *dev_id)
 	log_failure_reason(d);
 	subsystem_restart_dev(d->subsys);
 
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+	#ifdef CHECK_NV_DESTROYED_MI
+	if (strnstr(last_modem_sfr_reason, STR_NV_SIGNATURE_DESTROYED, strlen(last_modem_sfr_reason))) {
+		pr_err("errimei_dev: the NV has been destroyed, should restart to recovery\n");
+		schedule_delayed_work(&create_kobj_work, msecs_to_jiffies(1*1000));
+	}
+	#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
 	return IRQ_HANDLED;
 }
 
@@ -1314,12 +1429,25 @@ static struct platform_driver pil_tz_driver = {
 
 static int __init pil_tz_init(void)
 {
+	last_ssr_reason_entry = proc_create("last_mcrash", S_IFREG | S_IRUGO, NULL, &last_ssr_reason_file_ops);
+	if (!last_ssr_reason_entry) {
+	    printk(KERN_ERR "pil: cannot create proc entry last_mcrash\n");
+	}
 	return platform_driver_register(&pil_tz_driver);
 }
 module_init(pil_tz_init);
 
 static void __exit pil_tz_exit(void)
 {
+	if (last_ssr_reason_entry) {
+	    remove_proc_entry("last_mcrash", NULL);
+	    last_ssr_reason_entry = NULL;
+	}
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#ifdef CHECK_NV_DESTROYED_MI
+	schedule_work(&clean_kobj_work);
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
 	platform_driver_unregister(&pil_tz_driver);
 }
 module_exit(pil_tz_exit);

drivers/usb/phy/phy-msm-snps-hs.c
@@ -83,6 +83,13 @@
 #define USB_HSPHY_1P8_VOL_MAX			1800000 /* uV */
 #define USB_HSPHY_1P8_HPM_LOAD			19000	/* uA */
 
+#undef dev_dbg
+#define dev_dbg dev_info
+#undef pr_debug
+#define pr_debug pr_info
+
+unsigned long panel_info = 1;
+
 struct msm_hsphy {
 	struct usb_phy		phy;
 	void __iomem		*base;
@@ -802,10 +809,18 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 	if (IS_ERR(phy->phy_reset))
 		return PTR_ERR(phy->phy_reset);
 
-	phy->param_override_seq_cnt = of_property_count_elems_of_size(
+	pr_info("panel_info %x\n", panel_info);
+	if (panel_info == 1)
+		phy->param_override_seq_cnt = of_property_count_elems_of_size(
 					dev->of_node,
 					"qcom,param-override-seq",
 					sizeof(*phy->param_override_seq));
+	if (panel_info == 0)
+		phy->param_override_seq_cnt = of_property_count_elems_of_size(
+					dev->of_node,
+					"qcom,param-override-seq-no-panel",
+					sizeof(*phy->param_override_seq));
+
 	if (phy->param_override_seq_cnt > 0) {
 		phy->param_override_seq = devm_kcalloc(dev,
 					phy->param_override_seq_cnt,
@@ -819,10 +834,17 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 			return -EINVAL;
 		}
 
-		ret = of_property_read_u32_array(dev->of_node,
+		if (panel_info == 1)
+			ret = of_property_read_u32_array(dev->of_node,
 				"qcom,param-override-seq",
 				phy->param_override_seq,
 				phy->param_override_seq_cnt);
+		if (panel_info == 0)
+			ret = of_property_read_u32_array(dev->of_node,
+				"qcom,param-override-seq-no-panel",
+				phy->param_override_seq,
+				phy->param_override_seq_cnt);
+
 		if (ret) {
 			dev_err(dev, "qcom,param-override-seq read failed %d\n",
 				ret);
@@ -904,6 +926,24 @@ static int msm_hsphy_remove(struct platform_device *pdev)
 	msm_hsphy_enable_power(phy, false);
 	return 0;
 }
+
+#ifdef CONFIG_TARGET_PROJECT_K7T
+static int __init parameter_select(char *str)
+{
+	int ret = 0;
+
+	ret = kstrtol(str, 10, &panel_info);
+	if (ret < 0)
+		return ret;
+
+	if (panel_info > 1)
+		pr_err("can't get panel_info\n");
+
+	pr_info("get panel_info %x from cmdline\n", panel_info);
+	return 1;
+}
+__setup("panel_info=", parameter_select);
+#endif
 
 static const struct of_device_id msm_usb_id_table[] = {
 	{

mrsrimar22/android_kernel_xiaomi_sm6225

drivers/soc/qcom/subsys-pil-tz.c
@@ -33,6 +33,24 @@
 #define STOP_ACK_TIMEOUT_MS	1000
 #define CRASH_STOP_ACK_TO_MS	200
 
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/spinlock.h>
+static char last_ssr_reason[MAX_SSR_REASON_LEN] = "none";
+static struct proc_dir_entry *last_ssr_reason_entry;
+static DEFINE_SPINLOCK(last_ssr_reason_lock);
+
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#define CHECK_NV_DESTROYED_MI 1
+#ifdef CHECK_NV_DESTROYED_MI
+#include <linux/workqueue.h>
+#include <linux/slab.h>
+#define STR_NV_SIGNATURE_DESTROYED "CRITICAL_DATA_CHECK_FAILED"
+static char last_modem_sfr_reason[MAX_SSR_REASON_LEN] = "none";
+static DEFINE_SPINLOCK(last_modem_sfr_reason_lock);
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
+
 #define ERR_READY	0
 #define PBL_DONE	1
 
@@ -163,6 +181,105 @@ static struct msm_bus_scale_pdata scm_pas_bus_pdata = {
 	.usecase = scm_pas_bw_tbl,
 	.num_usecases = ARRAY_SIZE(scm_pas_bw_tbl),
 	.name = "scm_pas",
+};
+
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#ifdef CHECK_NV_DESTROYED_MI
+static struct kobject *checknv_kobj;
+static struct kset *checknv_kset;
+static const struct sysfs_ops checknv_sysfs_ops = {};
+
+static void kobj_release(struct kobject *kobj)
+{
+	kfree(kobj);
+}
+
+static struct kobj_type checknv_ktype = {
+	.sysfs_ops = &checknv_sysfs_ops,
+	.release = kobj_release,
+};
+
+static void checknv_kobj_clean(struct work_struct *work)
+{
+	if (checknv_kobj) {
+		kobject_uevent(checknv_kobj, KOBJ_REMOVE);
+		kobject_put(checknv_kobj);
+		checknv_kobj = NULL;
+	}
+	if (checknv_kset) {
+		kset_unregister(checknv_kset);
+		checknv_kset = NULL;
+	}
+}
+
+static void checknv_kobj_create(struct work_struct *work)
+{
+	int ret;
+
+	/* if already exists, log and return */
+	if (checknv_kobj || checknv_kset) {
+		pr_err("checknv: kobj/kset already exist, skipping create\n");
+		return;
+	}
+
+	checknv_kset = kset_create_and_add("checknv_errimei", NULL, NULL);
+	if (!checknv_kset) {
+		pr_err("checknv: kset creation failed\n");
+		checknv_kset = NULL;
+		return;
+	}
+
+	checknv_kobj = kzalloc(sizeof(*checknv_kobj), GFP_KERNEL);
+	if (!checknv_kobj) {
+		pr_err("checknv: kobject alloc failed\n");
+		kset_unregister(checknv_kset);
+		checknv_kset = NULL;
+		return;
+	}
+
+	checknv_kobj->kset = checknv_kset;
+	ret = kobject_init_and_add(checknv_kobj, &checknv_ktype, NULL, "%s", "errimei");
+	if (ret) {
+		pr_err("checknv: kobject_init_and_add failed: %d\n", ret);
+		kobject_put(checknv_kobj);
+		checknv_kobj = NULL;
+		kset_unregister(checknv_kset);
+		checknv_kset = NULL;
+		return;
+	}
+
+	kobject_uevent(checknv_kobj, KOBJ_ADD);
+}
+
+static DECLARE_DELAYED_WORK(create_kobj_work, checknv_kobj_create);
+static DECLARE_WORK(clean_kobj_work, checknv_kobj_clean);
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
+
+static int last_ssr_reason_proc_show(struct seq_file *m, void *v)
+{
+	unsigned long flags;
+	char buf[MAX_SSR_REASON_LEN];
+
+	spin_lock_irqsave(&last_ssr_reason_lock, flags);
+	strscpy(buf, last_ssr_reason, sizeof(buf));
+	spin_unlock_irqrestore(&last_ssr_reason_lock, flags);
+
+	seq_printf(m, "%s\n", buf);
+	return 0;
+}
+
+static int last_ssr_reason_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, last_ssr_reason_proc_show, NULL);
+}
+
+static const struct file_operations last_ssr_reason_file_ops = {
+	.owner	= THIS_MODULE,
+	.open	= last_ssr_reason_proc_open,
+	.read	= seq_read,
+	.llseek	= seq_lseek,
+	.release = single_release,
 };
 
 static uint32_t scm_perf_client;
@@ -822,8 +939,18 @@ static void log_failure_reason(const struct pil_tz_data *d)
 		return;
 	}
 
-	strlcpy(reason, smem_reason, min(size, (size_t)MAX_SSR_REASON_LEN));
+	strscpy(reason, smem_reason, min(size, (size_t)MAX_SSR_REASON_LEN));
 	pr_err("%s subsystem failure reason: %s.\n", name, reason);
+
+	spin_lock(&last_ssr_reason_lock);
+	snprintf(last_ssr_reason, sizeof(last_ssr_reason), "%s: %s", name, reason);
+	spin_unlock(&last_ssr_reason_lock);
+#ifdef CHECK_NV_DESTROYED_MI
+	spin_lock(&last_modem_sfr_reason_lock);
+	strscpy(last_modem_sfr_reason, smem_reason,
+		min(size, (size_t)MAX_SSR_REASON_LEN));
+	spin_unlock(&last_modem_sfr_reason_lock);
+#endif
 }
 
 static int subsys_shutdown(const struct subsys_desc *subsys, bool force_stop)
@@ -895,6 +1022,12 @@ static void subsys_crash_shutdown(const struct subsys_desc *subsys)
 static irqreturn_t subsys_err_fatal_intr_handler (int irq, void *dev_id)
 {
 	struct pil_tz_data *d = subsys_to_data(dev_id);
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#ifdef CHECK_NV_DESTROYED_MI
+	unsigned long flags;
+	bool nv_destroyed = false;
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
 
 	pr_err("Fatal error on %s!\n", d->subsys_desc.name);
 	if (subsys_get_crash_status(d->subsys)) {
@@ -905,6 +1038,21 @@ static irqreturn_t subsys_err_fatal_intr_handler (int irq, void *dev_id)
 	subsys_set_crash_status(d->subsys, CRASH_STATUS_ERR_FATAL);
 	log_failure_reason(d);
 	subsystem_restart_dev(d->subsys);
+
+/*2019.12.09 longcheer weipuchao add for checknv begin*/
+#ifdef CHECK_NV_DESTROYED_MI
+	spin_lock_irqsave(&last_modem_sfr_reason_lock, flags);
+	nv_destroyed = last_modem_sfr_reason[0] != '\0' &&
+		strnstr(last_modem_sfr_reason, STR_NV_SIGNATURE_DESTROYED,
+			strlen(last_modem_sfr_reason));
+	spin_unlock_irqrestore(&last_modem_sfr_reason_lock, flags);
+
+	if (nv_destroyed) {
+		pr_err("errimei_dev: the NV has been destroyed, should restart to recovery\n");
+		schedule_delayed_work(&create_kobj_work, msecs_to_jiffies(1000));
+	}
+#endif
+/*2019.12.09 longcheer weipuchao add for checknv end*/
 
 	return IRQ_HANDLED;
 }
@@ -1314,12 +1462,29 @@ static struct platform_driver pil_tz_driver = {
 
 static int __init pil_tz_init(void)
 {
+	last_ssr_reason_entry = proc_create("last_mcrash", 0444, NULL, &last_ssr_reason_file_ops);
+	if (!last_ssr_reason_entry)
+		printk(KERN_ERR "pil: cannot create proc entry last_mcrash\n");
+
 	return platform_driver_register(&pil_tz_driver);
 }
 module_init(pil_tz_init);
 
 static void __exit pil_tz_exit(void)
 {
+	if (last_ssr_reason_entry) {
+		proc_remove(last_ssr_reason_entry);
+		last_ssr_reason_entry = NULL;
+	}
+
+#ifdef CHECK_NV_DESTROYED_MI
+	cancel_delayed_work_sync(&create_kobj_work);
+	if (!work_pending(&clean_kobj_work))
+		schedule_work(&clean_kobj_work);
+	flush_work(&clean_kobj_work);
+#endif
+	/*2019.12.09 longcheer weipuchao add for checknv end*/
+
 	platform_driver_unregister(&pil_tz_driver);
 }
 module_exit(pil_tz_exit);

drivers/usb/phy/phy-msm-snps-hs.c
@@ -83,6 +83,16 @@
 #define USB_HSPHY_1P8_VOL_MAX			1800000 /* uV */
 #define USB_HSPHY_1P8_HPM_LOAD			19000	/* uA */
 
+// only for debug
+/*
+#undef dev_dbg
+#define dev_dbg dev_info
+#undef pr_debug
+#define pr_debug pr_info
+*/
+
+unsigned long panel_info = 1;
+
 struct msm_hsphy {
 	struct usb_phy		phy;
 	void __iomem		*base;
@@ -802,10 +812,18 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 	if (IS_ERR(phy->phy_reset))
 		return PTR_ERR(phy->phy_reset);
 
-	phy->param_override_seq_cnt = of_property_count_elems_of_size(
+	pr_info("panel_info %lx\n", panel_info);
+	if (panel_info == 1)
+		phy->param_override_seq_cnt = of_property_count_elems_of_size(
 					dev->of_node,
 					"qcom,param-override-seq",
 					sizeof(*phy->param_override_seq));
+	if (panel_info == 0)
+		phy->param_override_seq_cnt = of_property_count_elems_of_size(
+					dev->of_node,
+					"qcom,param-override-seq-no-panel",
+					sizeof(*phy->param_override_seq));
+
 	if (phy->param_override_seq_cnt > 0) {
 		phy->param_override_seq = devm_kcalloc(dev,
 					phy->param_override_seq_cnt,
@@ -819,10 +837,18 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 			return -EINVAL;
 		}
 
-		ret = of_property_read_u32_array(dev->of_node,
-				"qcom,param-override-seq",
-				phy->param_override_seq,
-				phy->param_override_seq_cnt);
+		if (panel_info == 1)
+			ret = of_property_read_u32_array(dev->of_node,
+					"qcom,param-override-seq",
+					phy->param_override_seq,
+					phy->param_override_seq_cnt);
+
+		if (panel_info == 0)
+			ret = of_property_read_u32_array(dev->of_node,
+					"qcom,param-override-seq-no-panel",
+					phy->param_override_seq,
+					phy->param_override_seq_cnt);
+
 		if (ret) {
 			dev_err(dev, "qcom,param-override-seq read failed %d\n",
 				ret);
@@ -904,6 +930,24 @@ static int msm_hsphy_remove(struct platform_device *pdev)
 	msm_hsphy_enable_power(phy, false);
 	return 0;
 }
+
+#ifdef CONFIG_TARGET_PROJECT_K7T
+static int __init parameter_select(char *str)
+{
+	int ret = 0;
+
+	ret = kstrtol(str, 10, &panel_info);
+	if (ret < 0)
+		return ret;
+
+	if (panel_info > 1)
+		pr_err("can't get panel_info\n");
+
+	pr_info("get panel_info %lx from cmdline\n", panel_info);
+	return 1;
+}
+__setup("panel_info=", parameter_select);
+#endif
 
 static const struct of_device_id msm_usb_id_table[] = {
 	{